8c060275c2a23e27fb80cdbbc756f14c71d6fa00
bdobry <bartoszdobry@gmail.com>
2015-12-01 11:11:09 +0100

    2 inputs working

--- sites/all/modules/phantomjs_capture/js/phantomjs_capture.js
+++ sites/all/modules/phantomjs_capture/js/phantomjs_capture.js
@@ -11,9 +11,10 @@ var address, output, size, selector;
 address = system.args[1];
 output = system.args[2];
 selector = system.args[3];
+onet = system.args[4];
 
 // Set the capture size to full HD.
-page.viewportSize = { width: 1920, height: 1080 };
+page.viewportSize = { width: 1020, height: 1080 };
 
 // Check if PDF should be rendered.
 if (system.args.length === 3 && system.args[1].substr(-4) === ".pdf") {
@@ -23,38 +24,7 @@ if (system.args.length === 3 && system.args[1].substr(-4) === ".pdf") {
 
 // Open the address and capture the screen after 200 mill-seconds (give the
 // screen the chance to run JS alters).
-page.open(address, function (status) {
-  if (status !== 'success') {
-    // Error fetching page.
-    console.log('500');
-    phantom.exit();
-  } else {
-    window.setTimeout(function () {
-      // Page fetch and timeout ran out... so capture the screen.
-      if (selector) {
-        var clipRect = page.evaluate(function (selector) {
-          var domelement = document.querySelector(selector);
-
-          if (domelement) {
-            return domelement.getBoundingClientRect();
-          } else {
-            return undefined;
-          }
-        }, selector);
-
-        if (clipRect) {
-          page.clipRect = {
-              top:    clipRect.top,
-              left:   clipRect.left,
-              width:  clipRect.width,
-              height: clipRect.height
-          };
-        }
-      }
-
-      page.render(output);
-      console.log('200');
-      phantom.exit();
-    }, 200);
-  }
+page.open(onet, function() {
+  page.render(output);
+  phantom.exit();
 });
